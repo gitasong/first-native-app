1b3074370cd02714c4987fac4ffed434a965522f
Nicole Freed <nicole.freed@gmail.com>
2017-09-28 18:06:33 -0700

    create Card class, render View component

--- components/tinder/Tinder.js
+++ components/tinder/Tinder.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
