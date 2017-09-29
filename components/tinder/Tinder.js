df826ab4a39251fb3c74fcf3423e50ac421ed573
Nicole Freed <nicole.freed@gmail.com>
2017-09-29 10:53:32 -0700

    import SwipeCards

--- components/tinder/Tinder.js
+++ components/tinder/Tinder.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SwipeCards from 'react-native-swipe-cards';
 
 class Card extends React.Component {
   render() {
