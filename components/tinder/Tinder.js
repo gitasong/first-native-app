b4c0ec67048bfd54aeb06acbbabebb1dd7ff7e23
Nicole Freed <nicole.freed@gmail.com>
2017-09-29 13:01:05 -0700

    delete NoMoreCards style

--- components/tinder/Tinder.js
+++ components/tinder/Tinder.js
@@ -15,7 +15,7 @@ class Card extends React.Component {
 class NoMoreCards extends React.Component {
   render() {
     return (
-      <View style={styles.card}>
+      <View>
         <Text>No More Jokes</Text>
       </View>
     );
